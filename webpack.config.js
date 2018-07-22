a81150c0005b3c0480c24d0fa4be4e83bba11837
Thomas Carmody <tcarmody90@yahoo.com>
2018-07-22 03:44:48 +0000

    Set up webpack, delete old scripts folder

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+console.log(path.join(__dirname, 'public')); // /home/ubuntu/workspace/indecision-app/src
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
