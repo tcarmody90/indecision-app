097fd013831cfe9242d42ee259ccb2da120a9a85
Thomas Carmody <tcarmody90@yahoo.com>
2018-07-22 18:44:01 +0000

    break app up into individual files

--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
-//import './utils.js';
-import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-// console.log("app.js is running!");
-// console.log(square(3));
-// console.log(add(3,5));
-// console.log(subtract(95,18));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(18));
-console.log(canDrink(20));
-console.log(isSenior(65));
-
-// Setting the default export and function
-// Grab the default and call it
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,14 @@ console.log(path.join(__dirname, 'public')); // /home/ubuntu/workspace/indecisio
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
   }
 };
